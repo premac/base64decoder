427d776493dd944b20bdcecbc24ab4992c7e5f17
Premysl Concka <concka@gmail.com>
2013-05-27 18:11:31 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,88 @@
+/*
+ * main.c
+ *
+ *  Created on: May 27, 2013
+ *      Author: nsbuild
+ */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+
+void build_decoding_table() {
+	int i;
+
+	decoding_table = malloc(256);
+    /* set undefined values to null */
+    memset(decoding_table, 0, 256 * sizeof(decoding_table[0]));
+
+    for (i = 0; i < 0x40; i++)
+        decoding_table[encoding_table[i]] = i;
+}
+
+unsigned char *base64_decode(const char *data,
+                    size_t input_length,
+                    size_t *output_length) {
+
+	int i, j;
+
+	if (data == NULL) {
+		return NULL;
+	}
+
+    if (decoding_table == NULL) build_decoding_table();
+
+    /* if (input_length % 4 != 0) return NULL; */
+
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char *decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+
+    for (i = 0, j = 0; i < input_length/4*4;) {
+
+    	//skip newline and other non-base64 symbols
+    	while (decoding_table[data[i]] == NULL) {
+    		i++;
+    	}
+
+        unsigned sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        unsigned sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        unsigned sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        unsigned sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+9
+        unsigned triple = (sextet_a << 3 * 6)
+                        + (sextet_b << 2 * 6)
+                        + (sextet_c << 1 * 6)
+                        + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+
+    return decoded_data;
+}
+
+int main(int argc, char **argv) {
+	char *s = "abc";
+	size_t output_length;
+
+	printf("Starting length: %d\n", strlen (s));
+	printf(base64_decode(s, strlen (s), &output_length));
+	printf("Output: %d", output_length);
+}
